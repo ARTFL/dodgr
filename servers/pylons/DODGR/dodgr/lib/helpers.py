6296d1a1101b025b953bbf80584eabf3a184a169
Russell Horton <russ@wordnik.com>
2010-08-30 21:46:39 -0700

    sanitize the definition html, so that tags get closed and the contents can't mess with the rest of the page

--- servers/pylons/DODGR/dodgr/lib/helpers.py
+++ servers/pylons/DODGR/dodgr/lib/helpers.py
@@ -5,3 +5,11 @@ available to Controllers. This module is available to templates as 'h'.
 """
 # Import helpers as desired, or define your own, ie:
 #from webhelpers.html.tags import checkbox, password
+
+import tidylib
+
+
+def sanitize_html(html):
+    document, errors = tidylib.tidy_document(html,
+                                options={'numeric-entities': 1})
+    return document
