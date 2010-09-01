c068a76e1c8b91dee0638b1d24842f31d868db70
Russell Horton <russ@wordnik.com>
2010-09-01 13:28:52 -0700

    pluralization helper to inflect words properly based on count of numerical specifier

--- servers/pylons/DODGR/dodgr/lib/helpers.py
+++ servers/pylons/DODGR/dodgr/lib/helpers.py
@@ -13,3 +13,28 @@ def sanitize_html(html):
     document, errors = tidylib.tidy_document(html,
                                 options={'numeric-entities': 1})
     return document
+
+
+def pluralize(word, count, plurals=None):
+    """Take a word and a count, and pluralize the word if the count is not
+    equal to 1.
+
+    Optionally take a plural form to use if the plural is not
+    formed by appending s.
+
+    If word is a list, add the suffix to each word in the list. This is handy
+    for French adjectives.
+
+    TODO -- this function needs to be set up for localization.
+    """
+    if type(word) == list:
+        words = word
+    else:
+        words = [word]
+
+    if count == 1:
+        return '1 ' + ' '.join(words)
+    else:
+        if not plurals:
+            plurals = [word + 's' for word in words]
+        return str(count) + ' ' + ' '.join(plurals)
