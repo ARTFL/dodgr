4344f652a2c15fae50924248e761360243000a77
timallen <tim@wordnik.com>
2010-11-10 13:12:39 -0600

    Edited dico names

--- servers/pylons/DODGR/dodgr/lib/app_globals.py
+++ servers/pylons/DODGR/dodgr/lib/app_globals.py
@@ -23,25 +23,25 @@ class Globals(object):
         """
 
         dicos = [(u'ACAD1932',
-                 u'Dictionnaire de L\'Académie française 8th edition '
+                 u'Dictionnaire de L\'Académie française 8e édition '
                  u'(1932-1935)'),
                  (u'LITTRE1872',
                  u'Émile Littré: Dictionnaire de la langue '
                  u'française (1872-1877)'),
                  (u'ACAD1835',
-                 u'Dictionnaire de L\'Académie française 6th edition '
+                 u'Dictionnaire de L\'Académie française 6e édition '
                  u'(1835)'),
                  (u'ACAD1798',
-                 u'Dictionnaire de L\'Académie française 5th edition '
+                 u'Dictionnaire de L\'Académie française 5e édition '
                  u'(1798)'),
                  (u'FERAUD1787',
                  u'Féraud: Dictionaire critique de '
                  u'la langue française (1787-1788)'),
                  (u'ACAD1762',
-                 u'Dictionnaire de L\'Académie française 4th edition '
+                 u'Dictionnaire de L\'Académie française 4e édition '
                  u'(1762)'),
                  (u'ACAD1694',
-                 u'Dictionnaire de L\'Académie française 1st edition '
+                 u'Dictionnaire de L\'Académie française 1re édition '
                  u'(1694)'),
                  (u'NICOT1606',
                  u'Jean Nicot: Thresor de la langue française '
