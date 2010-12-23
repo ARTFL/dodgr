6856fbcd4880d38d9911f6d0444b6f0157d2fff3
Clovis Gladstone <clovis@lafayette.(none)>
2010-12-23 17:58:10 -0600

    many cosmetic changes

--- servers/pylons/DODGR/dodgr/public/js/dodgr.js
+++ servers/pylons/DODGR/dodgr/public/js/dodgr.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
         } else {
             triangle.html('▽');
         }
-		$(this).next().toggle();
+		$(this).next().toggle(400);
 		return false;
 	});
 
@@ -31,10 +31,10 @@ $(document).ready(function() {
 	$('.word_reference_trigger').click(function() {
 	    if ($('#word_reference_container').html() == '') {
             $('#word_reference_container').html('<iframe width="320" height="600" src="http://mini.wordreference.com/mini/index.aspx?dict=fren&w=' + $('#main_headword_input').val() + '&u=1" name="WRmini"></iframe>');
-            $('.word_reference_trigger').html('Supprimer WordReference');
+            $('.word_reference_trigger').html('<span class="web_link">Supprimer WordReference</span>');
         } else {
             $('#word_reference_container').html('');
-            $('.word_reference_trigger').html('Activer WordReference');
+            $('.word_reference_trigger').html('<span class="web_link">Activer WordReference</span>');
         }
     });
 
