0670fdffc3a4459850cddc720399ee70d1ddd617
Clovis Gladstone <clovis@lafayette.(none)>
2011-01-04 14:38:38 -0600

    trying another fix here, MacOSX and its python implementation are getting on my nerves... previous two commits work on Linux, and so does this one.

--- sentences/examples.py
+++ sentences/examples.py
@@ -8,20 +8,20 @@ def get_sentences(sentence_db, word, db, limit=20):
     #TODO set the number of sentences before query?
     
     if sentence_db == 'corpasentences_utf8':
-        corpasentences = db.query("""SELECT id, content, score FROM corpasentences_utf8 WHERE headword = %s ORDER BY score DESC LIMIT %d""", (word, limit))
+        corpasentences = db.query("""SELECT id, content, score FROM corpasentences_utf8 WHERE headword = %s ORDER BY score DESC LIMIT %d""" % (word, limit))
         return corpasentences
         
     elif sentence_db == 'littresentences_utf8':
         littresentences = db.query("""SELECT id, content, source, score
                                     FROM littresentences_utf8
-                                    WHERE headword = %s ORDER BY score DESC LIMIT %d""", (word, limit))
+                                    WHERE headword = %s ORDER BY score DESC LIMIT %d""" % (word, limit))
         return littresentences
         
     elif sentence_db == 'websentences_utf8':
 
         websentences = db.query("""SELECT id, content, source, link, score
                                 FROM websentences_utf8
-                                WHERE headword = %s ORDER BY score DESC LIMIT %d""", (word, limit))
+                                WHERE headword = %s ORDER BY score DESC LIMIT %d""" % (word, limit))
         link_pattern = re.compile('(\w+\.)+\w+\/')
         for i in xrange(len(websentences)):
             link = websentences[i]['link']
